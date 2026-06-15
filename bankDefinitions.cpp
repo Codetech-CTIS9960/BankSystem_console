82edf0d410ecfbbe8788fdfc9a189394f3097a6c
Codetech-CTIS9960 <awesomerajput90073@gmail.com>
2026-06-15 19:17:37 +0530

    Create bankDefinitions.cpp

---
+++ bankDefinitions.cpp
@@ -0,0 +1,414 @@
+#include "bankDeclarations.hpp"
+bool fileExists(const std::string &filename)
+{
+    std::ifstream file(filename);
+    return file.good();
+}
+
+void clearScreen()
+{
+#ifdef _WIN32
+    {
+
+        std::system("cls");
+    }
+#else
+    {
+        std::system("clear");
+    }
+#endif
+}
+int fileIO::writeIntToFile(int value) const
+{
+    std::ofstream createAccountFile("logs/account.txt");
+    if (!fileExists("logs/account.txt"))
+    {
+        clearScreen();
+        printColoredText("\n\n\tProgram file(s) is/are missing! Generating new one, data loss! \n", ANSI_COLOR_RED);
+        printColoredText("\n\n\tYou must now try again! \n", ANSI_COLOR_GREEN);
+        std::ofstream createAccountFile("logs/account.txt");
+        createAccountFile << 5000;
+    }
+    std::string filename = "logs/account.txt";
+    std::ofstream file(filename);
+    if (!file)
+    {
+        printColoredText(std::format("Error Opening the File: {0}", filename), ANSI_COLOR_RED);
+        return 1;
+    }
+
+    file << value;
+    file.close();
+    return 0;
+}
+
+void fileIO::cool() const
+{
+    printColoredText("\n\n\tThis program was created by SharonIV0x86 aka Jasperr \n", ANSI_COLOR_GREEN);
+    printColoredText("\tA lot of efforts have been put into and this time i rewrote whole code from scratch after an year! \n", ANSI_COLOR_GREEN);
+    printColoredText("\tStill there are some weak points in the code and it looks very complex. Feel Free To Raise an Issue ;) \n", ANSI_COLOR_GREEN);
+    printColoredText("\tand its also very unoptimized, yes i suck at coding swy :( lol Thanks! \n ", ANSI_COLOR_GREEN);
+    printColoredText("\tIf the program exitted unexpectedly is possible you entered some wrong value or contact developer ;)  \n", ANSI_COLOR_GREEN);
+    printColoredText("\thttps://GitHub.com/SharonIV0X86 \n", ANSI_COLOR_GREEN);
+}
+
+int fileIO::readIntFromFile() const
+{
+
+    if (!fileExists("logs/account.txt"))
+    {
+        clearScreen();
+        printColoredText("\n\n\tProgram file is missing! Generating new one. \n) ", ANSI_COLOR_RED);
+        std::ofstream createAccountFile("logs/account.txt");
+        createAccountFile << 5000;
+        printColoredText("\n\n\tYou must now try again! \n", ANSI_COLOR_GREEN);
+    }
+    std::ifstream readInt("logs/account.txt");
+    readInt >> File->remainingBalance;
+    readInt.close();
+    return 0;
+}
+std::string computeHash(const std::string &input) {
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+    const EVP_MD *md = EVP_sha512();
+    EVP_DigestInit_ex(mdctx, md, nullptr);
+    EVP_DigestUpdate(mdctx, input.c_str(), input.length());
+    unsigned char digest[EVP_MAX_MD_SIZE];
+    unsigned int digestLength;
+    EVP_DigestFinal_ex(mdctx, digest, &digestLength);
+    EVP_MD_CTX_free(mdctx);
+    std::string hash;
+    for (unsigned int i = 0; i < digestLength; i++) {
+        char buf[4]; // Size 4 for null terminator
+        snprintf(buf, sizeof(buf), "%02x", digest[i]); // Use snprintf for safety
+        hash += buf;
+    }
+    return hash;
+}
+
+void checkForEmptyField(std::string field)
+{
+    if (field.empty())
+    {
+        printColoredText("\n\n\tthis field cannot be empty! \n", ANSI_COLOR_RED);
+        exit(0);
+    }
+}
+void humanVerification()
+{
+    static int count = 0;
+    if (count > 3)
+    {
+        printColoredText("\n\n\tYou have reached the maximum number of wrong attempts! Kindly try again later.\n", ANSI_COLOR_RED);
+        account.savingLogs(5);
+        exit(0);
+    }
+
+    unsigned char buffer[2 * sizeof(int)];  // Enough space for two integers
+    if (RAND_bytes(buffer, sizeof(buffer)) != 1) {
+        // Handle error if random number generation fails
+        std::cerr << "Error generating random numbers using OpenSSL!\n";
+        return;
+    }
+    int num1 = *reinterpret_cast<int*>(buffer);
+    int num2 = *reinterpret_cast<int*>(buffer + sizeof(int));
+    num1 = std::abs(num1) % 20;  // Ensure positive values between 0 and 19
+    num2 = std::abs(num2) % 20;
+
+    int result = num1 + num2;
+
+    std::cout << "\n\t\t|--------------------------------------------|"
+              << "\n\t\t|                                            |";
+    printColoredText(std::format("\n\t\t| VERIFICATION EQUATION -----> {0} + {1} \t     |", num1, num2), ANSI_COLOR_GREEN);
+    std::cout << "\n\t\t|Enter the EQUATION on the screen:           |"
+              << "\n\t\t|--------------------------------------------|\n";
+
+    std::cout << "~~~~~~~~~~~~~~~~> ";
+
+    int userInput;
+    std::cin >> userInput;
+
+    if (std::cin.fail() || result != userInput)
+    {
+        std::cin.clear();
+        printColoredText("\n\n\tPlease enter the result of the above equation correctly!\n", ANSI_COLOR_RED);
+        printColoredText(std::format("\n\n\tYou have {0} attempts left! ", (3 - count)), ANSI_COLOR_RED);
+        count += 1;
+        humanVerification();
+    }
+    else
+    {
+        clearScreen();
+        printColoredText("\n\n\tVerification successful!\n", ANSI_COLOR_GREEN);
+    }
+}
+
+bool isFileEmpty(const std::string &filename)
+{
+    std::ifstream file(filename);
+    if (!file)
+    {
+        printColoredText(std::format("Error Opening the File: {0} \n", filename), ANSI_COLOR_RED);
+        return false;
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        if (!line.empty())
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void Account::accountAccess()
+{
+    if (File->readIntFromFile() > 1000000 || isFileEmpty("logs/account.txt") || !fileExists("logs/account.txt") || File->remainingBalance < 5000)
+    {
+        printColoredText("\n\n\tDont be greedy bru ;) \n", ANSI_COLOR_MAGENTA);
+        File->writeIntToFile(5000);
+        File->readIntFromFile();
+    }
+    int counter = 0;
+    bool proceedTransaction = false;
+    std::cin.ignore();
+    printColoredText(std::format("\n\n\tWELCOME TO Cyprus National Bank, {0} \t Your Balance--> {1} ", File->permanentAccountName, File->remainingBalance),
+                     ANSI_COLOR_GREEN);
+    printColoredText("\n\n\tPress 1 to DEPOSIT money to your account \n\n\tPress 2 to WITHDRAW money from your account) \n\n\tPress 3 to DELETE current account \n\n\tPress 4 to read bank's policies \n\n\tPress 5 to exit and get to know something cool ! \n\n\tNote : Your account can hold max of 10, 00, 000 !\n\t\t-- -- -- --->",
+                     ANSI_COLOR_CYAN);
+    int choice = 0;
+    std::cin >> choice;
+    switch (choice)
+    {
+    case 1:
+    {
+        clearScreen();
+        printColoredText("\n\n\tTo DEPOSIT money to your account you must complete verification! \n", ANSI_COLOR_RED);
+        humanVerification();
+
+        while (counter < 3)
+        {
+            printColoredText(std::format("\n\n\tYour current remaining Balance---> {0} \n", File->remainingBalance), ANSI_COLOR_CYAN);
+            printColoredText("\n\n\tEnter the amount of money you want to DEPOSIT to your account. you cannot DEPOSIT more than 1,00,000 at a time!---> ", ANSI_COLOR_YELLOW);
+            counter++;
+            std::cin >> File->depositAmount;
+            if (File->depositAmount > 100000 || File->depositAmount < 5000 || File->depositAmount <= 0)
+            {
+                clearScreen();
+                printColoredText("\n\t1.) You cannot deposit more than 1,00,000 at a time. ", ANSI_COLOR_YELLOW);
+                printColoredText("\n\t2.) Minimum amount for a deposit must be greater than 5,000. You have to deposit more than 5,000. ", ANSI_COLOR_YELLOW);
+                printColoredText("\n\t3.) Your account can hold a maximum amount of 10,00,000 (1Million). ", ANSI_COLOR_YELLOW);
+                printColoredText("\n\n\tRead Cyprus National Bank's policies. Press enter to continue \n", ANSI_COLOR_YELLOW);
+                std::cin.get();
+                std::cin.get();
+            }
+            else if (counter > 3)
+            {
+                clearScreen();
+                printColoredText("\n\n\t Too Many Wrong Attempts! ", ANSI_COLOR_RED);
+                break;
+            }
+            else
+            {
+                proceedTransaction = true;
+                break;
+            }
+        }
+        if (proceedTransaction)
+        {
+            File->remainingBalance = File->depositAmount + File->remainingBalance;
+            File->writeIntToFile(File->remainingBalance);
+            printColoredText("\n\n\tMoney DEPOSIT Succesful! Press enter to continue! \n", ANSI_COLOR_GREEN);
+            std::cin.get();
+            savingLogs(3);
+            accountAccess();
+        }
+        break;
+    }
+    case 2:
+    {
+        clearScreen();
+        printColoredText("\n\n\tTo WITHDRAW money to your account you must complete verification! \n", ANSI_COLOR_RED);
+        humanVerification();
+
+        while (counter < 3)
+        {
+            clearScreen();
+            printColoredText(std::format("Your current remaining Balance---> {0} \n", File->remainingBalance), ANSI_COLOR_CYAN);
+            printColoredText("\n\n\tEnter the amount of MONEY you want to WITHDRAW. Entered amount must NOT be greater than your current remainingBalance!---> ", ANSI_COLOR_YELLOW);
+            std::cin >> File->withdrawAmount;
+            int rem = File->remainingBalance - File->withdrawAmount;
+            counter++;
+            if (rem < 5000 || File->remainingBalance <= 5000 || File->withdrawAmount > File->remainingBalance || File->withdrawAmount <= 0)
+            {
+                clearScreen();
+                printColoredText("\n\t1.) You cannot withdraw money more than your current remainingBalance. ", ANSI_COLOR_YELLOW);
+                printColoredText("\n\t2.) You cannot use the withdrawal system if your current remainingBalance is found to be 5,000 you must have remainingBalance more than 5,000 in order to withdraw. ", ANSI_COLOR_YELLOW);
+                printColoredText("\n\t3.) You cannot withdrawal an amount which can make your account remainingBalance become less than 5,000.", ANSI_COLOR_YELLOW);
+                printColoredText("\n\n\tYou have entered an invalid amount for WITHDRAWING, Please read Cyprus National Bank's policies. Press enter to continue. ", ANSI_COLOR_YELLOW);
+                std::cin.get();
+                std::cin.get();
+            }
+            else if (counter > 3)
+            {
+                clearScreen();
+                printColoredText("\n\n\t Too Many Wrong Attempts! ", ANSI_COLOR_RED);
+                break;
+            }
+            else
+            {
+                proceedTransaction = true;
+                break;
+            }
+        }
+        if (proceedTransaction)
+        {
+            File->remainingBalance = File->remainingBalance - File->withdrawAmount;
+            printColoredText("\n\n\tMoney has been WITHDRAWN SUCCESSFULLY! \n", ANSI_COLOR_GREEN);
+            File->writeIntToFile(File->remainingBalance);
+            savingLogs(4);
+            accountAccess();
+        }
+        break;
+    }
+    case 3:
+    {
+        std::ifstream readData("logs/output.txt");
+        clearScreen();
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        printColoredText("\n\n\tWARNING, YOUR ACCOUNT DATA WILL BE DELETED FOREVER! ", ANSI_COLOR_RED);
+
+        printColoredText("\n\n\tEnter the account name --> ", ANSI_COLOR_YELLOW);
+        std::getline(std::cin, File->accountName);
+        checkForEmptyField(File->accountName);
+        printColoredText("\n\n\tEnter your account password--> ", ANSI_COLOR_YELLOW);
+        std::getline(std::cin, File->password);
+
+        checkForEmptyField(File->password);
+        printColoredText("\n\n\tEnter your NUMERIC PIN--> ", ANSI_COLOR_YELLOW);
+        std::getline(std::cin, File->pin);
+        checkForEmptyField(File->pin);
+
+        File->permanentAccountName = File->accountName;
+        File->hashedPassword = computeHash(File->password);
+        File->pin = File->pin;
+        if (File->permanentAccountName == File->readAcn && File->hashedPassword == File->readPassword && File->pin == File->readPin)
+        {
+            clearScreen();
+            std::vector<std::string> filenames = {"logs/output.txt", "logs/logs.txt", "logs/account.txt"};
+            for (auto ae : filenames)
+            {
+
+                std::ofstream ofs(ae, std::ios::trunc);
+                ofs.close();
+            }
+            printColoredText("\n\n\t YOUR ACCOUNT HAS BEEN DELETED PERMANENTYL! \n", ANSI_COLOR_GREEN);
+            savingLogs(6);
+            exit(0);
+        }
+        else
+        {
+            clearScreen();
+            printColoredText("\n\n\tCredentials Dont match Failed To Delete Account. \n", ANSI_COLOR_RED);
+            accountAccess();
+        }
+        break;
+    }
+
+    case 4:
+    {
+        clearScreen();
+        printColoredText("\n\n\t\t\t\tDEPOSITS ", ANSI_COLOR_RED);
+        printColoredText("\n\t1.) You cannot deposit more than 1,00,000 at a time. ", ANSI_COLOR_GREEN);
+        printColoredText("\n\t2.) Minimum amount for a deposit must be greater than 5,000. You have to deposit more than 5,000. ", ANSI_COLOR_GREEN);
+        printColoredText("\n\t3.) Your account can hold a maximum amount of 10,00,000 (1Million). ", ANSI_COLOR_GREEN);
+
+        printColoredText("\n\n\t\t\t\tWITHDRAWALS ", ANSI_COLOR_RED);
+        printColoredText("\n\t1.) You cannot withdraw money more than your current remainingBalance. ", ANSI_COLOR_GREEN);
+        printColoredText("\n\t2.) You cannot use the withdrawal system if your current remainingBalance is found to be 5,000 you must have remainingBalance more than 5,000 in order to withdraw. ", ANSI_COLOR_GREEN);
+        printColoredText("\n\t3.) You cannot withdrawal an amount which can make your account remainingBalance become less than 5,000. For example if you have a remainingBalance of 10,000 you cannot withdraw 8,000. ", ANSI_COLOR_GREEN);
+
+        printColoredText("\n\n\t\t\t\tACCOUNT DELETION ", ANSI_COLOR_RED);
+        printColoredText("\n\tThis is a serious case, if you delete your account then you will LOST ACCESS to it FOREVER! Tread Carefully. ", ANSI_COLOR_GREEN);
+
+        printColoredText("\n\n\t\t\t\tIMPORTANT! ", ANSI_COLOR_RED);
+        printColoredText("\n\tOn the account of suspicious activity Bank holds the right and authority to reset your account remainingBalance to 5,000 or even delete your bank account ", ANSI_COLOR_GREEN);
+        printColoredText("\n\tIf you are using Cyprus National Bank's services then you are agreeing to these policies .", ANSI_COLOR_GREEN);
+        printColoredText("\n\n\tPress enter to continue. ", ANSI_COLOR_GREEN);
+        printColoredText("\n\t\t\t\t\t\t\t~Cyprus National Bank", ANSI_COLOR_CYAN);
+        printColoredText("\n\t\t\t\t\t\t\t~JasperrBru ", ANSI_COLOR_CYAN);
+        std::cin.get();
+        accountAccess();
+        break;
+    }
+
+    case 5:
+    {
+        break;
+    }
+    default:
+    {
+        printColoredText("\n\n\tPlease enter a valid choice! ", ANSI_COLOR_RED);
+        break;
+    }
+    }
+}
+void Account ::savingLogs(int logAction) const {
+    std::ofstream log("logs/logs.txt", std::ios::app);
+    std::time_t currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    char timeString[100];
+    std::strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", std::localtime(&currentTime));
+    switch (logAction)
+    {
+    case 1:
+        log << timeString << " User Named : " << File->permanentAccountName << " [+][+]CREATED an account! \n";
+        break;
+    case 2:
+        log << "\n\n " << timeString << " User: " << File->permanentAccountName << " LOGGED INTO[+]  their account \n";
+        break;
+    case 3:
+        log << timeString << " User Named: " << File->permanentAccountName << " DEPOSITED[+][+] " << File->depositAmount << " NEW BALANCE " << File->remainingBalance << " \n";
+        break;
+    case 4:
+        log << timeString << " User Named: " << File->permanentAccountName << " WITHDRAWN[-][-] " << File->withdrawAmount << " remaining remainingBalance " << File->remainingBalance << " \n";
+        break;
+    case 5:
+        log << timeString << "User Named " << File->permanentAccountName << " tried logging into their account with WRONG CREDENTIALS[x][x] \n";
+        break;
+    case 6:
+        log << timeString << " User Named: " << File->permanentAccountName << " DELETED their account. \n";
+        break;
+    case 7:
+        log << timeString << " User Named: " << File->permanentAccountName << " LOGGED OUT of their account\n\n";
+
+    default:
+        break;
+    }
+    log.close();
+}
+void Account::checkCredentials()
+{
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::ifstream readData("logs/output.txt");
+    readData >> File->readAcn >> File->readPassword >> File->readPin;
+
+    if (File->permanentAccountName == File->readAcn && File->hashedPassword == File->readPassword && File->pin == File->readPin)
+    {
+        clearScreen();
+        printColoredText("\n\n\tRedirecting to your account now! Press Enter! \n", ANSI_COLOR_GREEN);
+        account.savingLogs(2);
+
+        accountAccess();
+    }
+    else if (File->permanentAccountName != File->readAcn || File->hashedPassword != File->readPassword || File->pin != File->readPin)
+    {
+        clearScreen();
+        printColoredText("\n\n\tCredentials Dont match kindly retry! This event will be recorded! \n", ANSI_COLOR_RED);
+        savingLogs(5);
+        exit(0);
+    }
+}
